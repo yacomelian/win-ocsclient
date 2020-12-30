cc214c0398c4f47fd4b592fcf75dcb06bb2ec889
unknown <yacomelian@gmail.com>
2020-12-30 09:37:45 +0000

    Version 20201230-0938

---
+++ OCSClient/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices.ComTypes;
+
+
+// https://github.com/vvvv/vvvv-sdk/blob/develop/common/src/core/Utils/OSC/OSCMessage.cs
+namespace OCSClient
+{
+    class Program
+    {
+
+        static void printUsage() {
+            Console.WriteLine("");
+            Console.WriteLine("Uso: OCSClient [host:port] <path> [option]");
+            Console.WriteLine("");
+            Console.WriteLine("Examples: OCSClient");
+            Console.WriteLine(" OCSClient 127.0.0.1:8181 /h2r-graphics/lower-third \"Title 1\"");
+            Console.WriteLine(" OCSClient 192.168.5.3:8181 /h2r-graphics/lower-third \"Title 1\"");
+            Console.WriteLine(" OCSClient /h2r-graphics/lower-third \"Title 1\"");
+
+            Console.WriteLine("");
+        }
+        static string[] validateInput (string[] args) {
+            //Console.WriteLine("Validating input of {0} args", args.Length);
+            string [] errorString = new string[] { };
+            if (args.Length == 0)
+            {
+                printUsage();
+                return errorString;
+            }
+
+            /*for (int i = 0; i < args.Length; i++) { 
+                Console.WriteLine("Validating input {0}: {1} ", i, args[i]);
+            }*/
+
+            string host = "127.0.0.1";
+            string port = "8181";
+            string path = "";
+            string action = "";
+            // Si envian host y puerto
+            if (args[0].Contains(":"))
+            {
+                string[] subs = args[0].Split(":");
+                host = subs[0];
+                port = subs[1];
+            }
+            else if (args[0].Contains("/"))
+            {
+                path = args[0];
+                if (args.Length == 2)
+                {
+                    action = args[1];
+                }
+            }
+            else {
+                Console.WriteLine("Opcion 1");
+                printUsage();
+                return errorString;
+            }
+
+            if (args.Length > 1) { 
+                if (args[0].Contains("/"))
+                {
+                    path = args[0];
+                    if (args.Length == 2) {
+                        action = args[1];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Opcion 2");
+                    printUsage();
+                    return errorString;
+                }
+            }
+            //string[] ret = new string[] { "127.0.0.1", "8181", "uri", "action" };
+            string[] ret = new string[] { host, port, path, action };
+
+
+            return ret;
+        }
+
+        static void Udpsend(string host, int port, string uri, string action) {
+            var client = new System.Net.Sockets.UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse(host), port); // endpoint where server is listening
+            client.Connect(ep);
+
+            // send data
+            client.Send(new byte[] { 1, 2, 3, 4, 5 }, 5);
+
+            // then receive data
+            try
+            {
+                var receivedData = client.Receive(ref ep);
+            }
+            catch (System.Net.Sockets.SocketException e) {
+                Console.WriteLine("Exception: {0}", e.Source);
+            }
+            Console.Write("receive data from " + ep.ToString());
+
+            Console.Read();
+
+        }
+        static int Main(string[] args)
+        {
+            string[] param = new string[] { };
+            param = validateInput(args);
+            if (param.Length == 4) {
+                //Console.WriteLine("Send  to {0}:{1}, with path {2}  and action {3}", param[0], param[1], param[2], param[3]);
+                var host = param[0];
+                var port = int.Parse(param[1]);
+                var path = param[2];
+                var action = param[3];
+
+                //var message = new SharpOSC.OscMessage("/h2r-graphics/clear", 23, 42.01f, "hello world");
+                //var sender = new SharpOSC.UDPSender("127.0.0.1", 8181);
+                
+
+                var message = new SharpOSC.OscMessage(path, action);
+                var sender = new SharpOSC.UDPSender(host, port);
+                sender.Send(message);
+                Console.WriteLine("Envío to {0}:{1}, with path {2}  and action {3}", param[0], param[1], param[2], param[3]);
+                Console.WriteLine("");
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
---
+++ OCSClient/Scripts/tests.ps1
@@ -0,0 +1,251 @@
+﻿
+$appPath = "..\bin\Debug\netcoreapp3.1\";
+
+# Para no cargar el path indefinidamente
+if ($env:Path -notlike "*"+$appPath+"*") {
+	"No lo contiene"
+	$env:Path += $appPath + ";" 
+} else {
+	"SIN MODIFICAR PATH"
+}
+
+$commands = @(
+	"/h2r-graphics/logo show",
+	"/h2r-graphics/logo hide",
+	"/h2r-graphics/clear"
+
+)
+
+foreach ($c in $commands)  {
+	OCSClient.exe $c
+	start-sleep 3
+}
+
+
+exit
+
+OCSClient.exe /h2r-graphics/logo show
+
+start-sleep 5
+
+OCSClient.exe /h2r-graphics/logo hide
+start-sleep 5
+
+OCSClient.exe /h2r-graphics/logo show
+
+start-sleep 5
+
+
+
+# Clear all graphics (OSC Message)
+#Path: /h2r-graphics/clear
+
+OCSClient.exe /h2r-graphics/clear
+
+# Lower Thirds Show
+#Path: /h2r-graphics/lower-third
+#String: 1 (replace with number of lower third you want to show)
+#Lower Thirds Hide
+
+OCSClient.exe /h2r-graphics/lower-third "Lower hide"
+
+#Type: OSC String
+
+#Path: /h2r-graphics/lower-third
+#String: hide
+OCSClient.exe /h2r-graphics/lower-third hide
+
+
+# Ticker Show
+#Type: OSC String
+#Path: /h2r-graphics/ticker
+#String: show
+OCSClient.exe /h2r-graphics/ticked hide
+
+<#
+Ticker Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/ticker
+
+String: hide
+Timer Show
+
+Type: OSC String
+
+Path: /h2r-graphics/timer
+
+String: show
+Timer Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/timer
+
+String: hide
+Custom Up Timer
+
+Type: OSC String
+
+Path: /h2r-graphics/timer-custom-up
+
+String: hh:mm (e.g. 01:00 = 1 hour timer)
+Custom Down Timer
+
+Type: OSC String
+
+Path: /h2r-graphics/timer-custom-down
+
+String: hh:mm (e.g. 00:01 = 1 minute timer)
+Custom Down to Time of Day
+
+Type: OSC String
+
+Path: /h2r-graphics/timer-custom-down-time-of-day
+
+String: hh:mm (e.g. 13:00 = Count down to 13:00)
+Current Time of Day
+
+Type: OSC String
+
+Path: /h2r-graphics/timer-current-time
+Stopwatch
+
+Type: OSC String
+
+Path: /h2r-graphics/stopwatch
+Pause time
+
+Type: OSC String
+
+Path: /h2r-graphics/timer
+
+String: pause
+Resume time
+
+Type: OSC String
+
+Path: /h2r-graphics/timer
+
+String: resume
+Set pre-timer custom message
+
+Type: OSC String
+
+Path: /h2r-graphics/timer-pre-message
+
+String: Message you want to set
+Logo Show
+
+Type: OSC String
+
+Path: /h2r-graphics/logo
+
+String: show
+Logo Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/logo
+
+String: hide
+Message Show
+
+Type: OSC String
+
+Path: /h2r-graphics/message
+
+String: show
+Message Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/message
+
+String: hide
+Display Custom Message
+
+Type: OSC String
+
+Path: /h2r-graphics/message
+
+String: Message that you want to show
+Break Show
+
+Type: OSC String
+
+Path: /h2r-graphics/break
+
+String: show
+Break Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/break
+
+String: hide
+Chat Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/chat
+
+String: hide
+Image Show Next
+
+Type: OSC String
+
+Path: /h2r-graphics/image
+
+String: next
+Image Show Previous
+
+Type: OSC String
+
+Path: /h2r-graphics/image
+
+String: previous
+Image Show Specific
+
+Type: OSC String
+
+Path: /h2r-graphics/image
+
+String: 1 (Where '1' will show the first image)
+Image Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/image
+
+String: hide
+Score Show
+
+Type: OSC String
+
+Path: /h2r-graphics/score
+
+String: show
+Score Hide
+
+Type: OSC String
+
+Path: /h2r-graphics/score
+
+String: hide
+Increase Team 1 score by 10 points
+
+Type: OSC String
+
+Path: /h2r-graphics/score-team-1
+
+String: 10
+Decrease Team 2 score by 5 points
+
+Type: OSC String
+
+Path: /h2r-graphics/score-team-2
+
+String: -5 
+#>
